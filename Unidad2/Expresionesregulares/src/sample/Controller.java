cfb877117d4f604c62ed4c655ad65109130532ea
Arath20 <71733853+Arath20@users.noreply.github.com>
2020-11-18 21:36:39 -0600

    Practica1
    El trabajo de practica1 lo hice en el mismo programa de Expresionesregulares

--- Unidad2/Expresionesregulares/src/sample/Controller.java
+++ Unidad2/Expresionesregulares/src/sample/Controller.java
@@ -19,7 +19,10 @@ public class Controller {
 
     @FXML
     protected void initialize() {
-        ListaEjer.getItems().addAll("Ejercicio 1", "Ejercicio 2", "Validacion password");
+        ListaEjer.getItems().addAll("Ejercicio 1", "Ejercicio 2", "Validacion Grupo",
+                "Palabra Guion", "No comenzar con db", "Terminar con count","Pancho Villa",
+                "MAC ADDRESS", "Mayuscula-Minuscula","Numero Binario", "Operacion 1 digito",
+                "No Control","CURP");
     }
         public void procesar (ActionEvent event){
             int seleccion = ListaEjer.getSelectionModel().getSelectedIndex();
@@ -36,7 +39,49 @@ public class Controller {
                  break;
              }
              case 2: {
-                 expresion="^-([A-Za-z0-9]{6,})";
+                 expresion="^(ISC)-([1-9])[A|B]$";
+                 break;
+             }
+             case 3: {
+                 expresion="([A-Za-z]{1,})[_]([A-Za-z]{1,})";
+                 break;
+             }
+             case 4: {
+                 expresion="([^db])[A-Za-z]{1,}";
+                 break;
+             }
+             case 5: {
+                 expresion="^(SUMA)-([1-9]{1,})(count$)";
+                 break;
+             }
+             case 6: {
+                 expresion="([A-Za-z]{1,})[\\s]([A-Za-z]{1,})";
+                 break;
+             }
+             case 7: {
+                 expresion="([A-Z0-9]{1,})-([0-9]{1,})-([A-Z0-9]{1,})-([A-Z]{1,})-([0-9A-Z]{1,})-([0-9]{1,})";
+                 break;
+             }
+             case 8: {
+                 expresion="(^[A-Z]{1,}+[a-z]{1,}$)";
+                 break;
+             }
+             case 9: {
+                 expresion="[0-1]{1,8}";
+                 break;
+             }
+             case 10: {
+                 expresion="([1-9]{1,})[+]([1-9]{1,})";
+                 break;
+             }
+             case 11: {
+                 expresion = "(^[0-9]{2})+[A-Z]{1,}+[0-9]{1,}";
+                 break;
+             }
+             case 12: {
+                 expresion="\"^[A-Z]{1}[AEIOU]{1}[A-Z]{2}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|20-9|3[0-3])" +
+                           "[HM]{1}(AS|BC|BS|CC|CS|CH|CL|CM|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)" +
+                           "[B-DF-HJ-NP-TV-Z]{3}[0-9A-Z]{1}[0-9]{1}$";
                  break;
              }
          }
