55d0727cb87461727840fa13189eb0032e7804a3
Arath20 <71733853+Arath20@users.noreply.github.com>
2020-11-23 21:54:39 -0600

    Practica 2

--- Unidad2/Expresionesregulares/src/sample/Controller.java
+++ Unidad2/Expresionesregulares/src/sample/Controller.java
@@ -22,7 +22,8 @@ public class Controller {
         ListaEjer.getItems().addAll("Ejercicio 1", "Ejercicio 2", "Validacion Grupo",
                 "Palabra Guion", "No comenzar con db", "Terminar con count","Pancho Villa",
                 "MAC ADDRESS", "Mayuscula-Minuscula","Numero Binario", "Operacion 1 digito",
-                "No Control","CURP");
+                "No Control","CURP", "Precio", "Tarjeta de Credito", "HTML", "Color Hexadecimal", "URL",
+                "Query SQL","Secuencia If","Salto de linea","URL YouTube");
     }
         public void procesar (ActionEvent event){
             int seleccion = ListaEjer.getSelectionModel().getSelectedIndex();
@@ -82,6 +83,42 @@ public class Controller {
                  expresion="\"^[A-Z]{1}[AEIOU]{1}[A-Z]{2}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|20-9|3[0-3])" +
                            "[HM]{1}(AS|BC|BS|CC|CS|CH|CL|CM|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)" +
                            "[B-DF-HJ-NP-TV-Z]{3}[0-9A-Z]{1}[0-9]{1}$";
+                 break;
+             }
+             case 13: {
+                 expresion = "^(\\$)([0-9]{1,})[\\.]([0]{1,})(mxn$)";//Precio
+                 break;
+             }
+             case 14: {
+                 expresion="^3[47][0-9]{1,}$"; //Tarjeta
+                 break;
+             }
+             case 15: {
+                 expresion="^(<p>)([A-Za-z]{1,})(</p>)$"; //HTML
+                 break;
+             }
+             case 16: {
+                 expresion=""; //PENDIENTE COLOR HEXADECIMAL (CON ESTE SI NO SUPE NI QUE SHOW )
+                 break;
+             }
+             case 17: {
+                 expresion="^(http://)([\\w]{1,})[\\.]([a-z0-9]{1,})(.com)$"; //URL
+                 break;
+             }
+             case 18: {
+                 expresion=""; // PENDIENTE SQL
+                 break;
+             }
+             case 19: {
+                 expresion=""; // PENDIENTE IF
+                 break;
+             }
+             case 20: {
+                 expresion="^([A-Za-z0-9]{1,})[\n]([A-Za-z0-9]{1,})"; //FRASE SALTO DE LINEA
+                 break;
+             }
+             case 21: {
+                 expresion = "^(http://)(www.)(Youtube.com/watch=)([A-Za-z0-9]{1,})"; //URL YOUTUBE
                  break;
              }
          }
